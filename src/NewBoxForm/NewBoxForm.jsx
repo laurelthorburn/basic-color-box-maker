86536348ca22aa47008412be79e2d33c875fdba3
Laurel Thorburn <laurelthorburn@gmail.com>
2022-04-11 20:16:26 -0600

    Updated package to reflect newer react vesion using functional not class based

--- src/NewBoxForm/NewBoxForm.jsx
+++ src/NewBoxForm/NewBoxForm.jsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
 
 //this component should render a form that when submitted, creates a new Box. You should be able to specify the Box's width, height, and background color. When the form is submitted, clear the inputs values
 
 const NewBoxForm = () => {
+
+const [width, setWidth] = useState("");
+const [height, setHeight] = useState("");
+const [backgroundColor, setBackgroundColor] = useState("");
+
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log("You clicked submit")
@@ -21,6 +26,7 @@ const NewBoxForm = () => {
         <input type="text" id="backgroundColor" />
         <button>Create Your Box 🪄</button>
       </form>
+      
     </>
   );
 };
